1663d5aec2c9acd55636daf96c25504d735386dd
Robert Razey <robert.x.razey.-nd@disney.com>
2018-01-02 20:42:16 -0800

    incremental

--- src/main/java/net/razey/cointracker/enums/Currency.java
+++ src/main/java/net/razey/cointracker/enums/Currency.java
@@ -1,6 +1,17 @@
 package net.razey.cointracker.enums;
 
 public enum Currency {
+    //crypto
+    BTC,
+    ETH,
+    LTC,
+    XRP,
+    XRB,
+    SC,
+    NEO,
+    VTC,
+
+    //fiat
     USD,
     CAD,
     EUR
---
+++ src/main/java/net/razey/cointracker/model/Transaction.java
@@ -0,0 +1,23 @@
+package net.razey.cointracker.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import net.razey.cointracker.enums.Currency;
+import org.joda.time.DateTime;
+
+@Getter
+@Setter
+public class Transaction {
+
+    DateTime dateTime;
+
+    Currency currencyPurchased;
+    Float purchasedQuantity;
+
+    Currency currencySold;
+    Float soldQuantity;
+
+    Currency currentPriceCurrency;
+    Float currentPrice;
+
+}
